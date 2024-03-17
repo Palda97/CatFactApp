0a617313ee65985dd896d820cf96fb958bb9b98a
David Paleček <palecekdavid@gmail.com>
2024-03-17 01:11:03 +0100

    Upgrade version from 1.0 to 1.1 (2)

--- buildSrc/src/main/kotlin/AppConfig.kt
+++ buildSrc/src/main/kotlin/AppConfig.kt
@@ -4,8 +4,8 @@ object AppConfig {
     const val compileSdk = 34
     const val minSdk = 26
     const val targetSdk = 34
-    const val versionCode = 1
-    const val versionName = "1.0"
+    const val versionCode = 2
+    const val versionName = "1.1"
     const val testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     val javaVersion = JavaVersion.VERSION_17
     const val jvmTarget = "17"
